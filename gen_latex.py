31312d9bfcb1027d4b69f914ae3ebb345e693a89
Ian McAulay <mcaulay.ian@gmail.com>
2017-09-30 13:33:37 -0700

    Multiple chapters separated by newline now supported

--- gen_latex.py
+++ gen_latex.py
@@ -1,27 +1,27 @@
 import time
 
-test1 = "Exercises 10: 4, 6, 12, 16, 20, 22, 24, 39, 40, 41, 43, 44"
 
 def make_path_name():
-    return "tmp/" + time.strftime("%Y%m%d-%H%M%S") + ".txt"
+    return "tmp/" + time.strftime("%Y%m%d-%H.%M.%S") + ".txt"
 
 
-def gen_latex(txt, path=make_path_name()):
+def write_latex(txt, path=make_path_name()):
 
-
-
-    lst = text_to_lst(txt)
     f = open(path, 'w')
     f.write("\section{Textbook Exercises}\n")
-    for exercise in lst:
-
-        block = make_latex_block(exercise)
-        f.write(block)
-
+    txt_lst = txt.split("\n")
+    for chap_txt in txt_lst:
+        write_chapter_latex(chap_txt, f)
 
     f.close()
-    
 
+
+def write_chapter_latex(chap_txt, f):
+
+    lst = text_to_lst(chap_txt)
+    for exercise in lst:
+        block = make_latex_block(exercise)
+        f.write(block)
 
 
 def text_to_lst(txt):
@@ -38,18 +38,26 @@ def text_to_lst(txt):
     return lst
 
 def make_latex_block(exercise):
-    # Desired about for input "10.1":
+    # Desired about for input "10.1" (including newline after \end):
     # \begin{mdframed}
     # \textbf{10.6}
     # 
     # 
     # \end{mdframed}
+    # 
 
     block = "\\begin{mdframed}\n"
     block += "\\textbf{" + exercise + "}\n"
     block += "\n\n"
-    block += "\\end{mdframed}\n"
+    block += "\\end{mdframed}\n\n"
     return block
 
-
-lst1 = text_to_lst(test1)
+def run_tests():
+    # Single chapter
+    test1 = "Exercises 10: 4, 6, 12, 16, 20, 22, 24, 39, 40, 41, 43, 44"
+    write_latex(test1)
+    # Two chapters
+    f2 = open("test/multiline.txt")
+    test2 = f2.read()
+    f2.close()
+    write_latex(test2)
